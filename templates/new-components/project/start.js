458cb4f2f16d1eccec9bf67f790b242c344f7761
James W. Lane <james.w.lane@mac.com>
2018-12-17 16:18:53 -0600

    fix(templates): Fixed start.js

--- templates/new-components/project/start.js
+++ templates/new-components/project/start.js
@@ -1,19 +1,25 @@
 "use strict";
 
 var statusBoard;
+const options = {
+  port: process.env.ATLASBOARD_PORT || process.env.STATUS_BOARD_PORT || 3000,
+  install: false
+};
+
+function callback(err) {
+  if (err) {
+    throw err;
+  }
+}
 
 try {
-  statusBoard = require('status-board');
+  statusBoard = require('status-board').default;
 } catch(e) {
-  console.log('Ups!, no Status Board module could be found locally, so you can not run this script yet!');
+  console.log('Ups!, no status-board module could be found locally, so you can not run this script yet!');
   console.log('You can either:');
   console.log(' a) install Status Board locally: `npm install status-board --save`. Then rerun this script.');
   console.log(' b) run this wallboard using the global Status Board binary: `status-board start 3000`');
   process.exit();
 }
 
-statusBoard({port: process.env.ATLASBOARD_PORT || process.env.STATUS_BOARD_PORT || 3000, install: true}, function (err) {
-  if (err) {
-    throw err;
-  }
-});
+statusBoard(options, callback);
