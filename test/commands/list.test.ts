6212c288c9c8932ec39828b671fc5ac196cf6928
James W. Lane <james.w.lane@mac.com>
2018-12-17 19:11:42 -0600

    ci(CircleCI): Fixed pipeline build

--- test/commands/list.test.ts
+++ test/commands/list.test.ts
@@ -1,10 +1,17 @@
-import { expect, test } from '@oclif/test';
+import { test } from '@oclif/test';
+
+import * as list from '../../src/list';
 
 describe('list', () => {
+  beforeEach(() => {
+    jest.spyOn(list, 'list').mockImplementation(() => {
+    });
+  });
+
   test
     .stdout()
     .command(['list'])
     .it('runs hello', (ctx) => {
-      expect(ctx.stdout).to.contain('hello world');
+      expect(list.list).toHaveBeenCalled();
     });
 });
